9421675bb15fe232fbac0dd09cdf32f7aa3a5e14
Paulo-Fraga <sarah11monyque@gmail.com>
2024-06-09 15:10:12 -0300

    Subindo ajustes

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -3,7 +3,10 @@ import { Variants, motion } from "framer-motion";
 import Image from "next/image";
 import { Dispatch, SetStateAction, Suspense, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import './menu.css';
+import './menu.scss';
+import { findItem, resetQtdItem, somaMaisUm, subtraiUm } from "../utils/math.utils";
+import { formatPrice } from "../utils/currency.utils";
+import { SpanPrice } from "../card/card";
 
 const TituloMenuAndCloseStyle = styled.div({
     display: 'flex',
@@ -19,7 +22,7 @@ const TituloMenu = styled.h3({
 })
 
 const CardMenuStyled = styled.div({
-    width: '385px',
+    width: '100%',
     height: '95px',
     display: 'flex',
     alignItems: 'center',
@@ -28,6 +31,7 @@ const CardMenuStyled = styled.div({
     borderRadius: '8px',
     margin: '15px 0px',
     color: '#000000',
+    position: 'relative'
 })
 
 const CardName = styled.div({
@@ -37,7 +41,8 @@ const CardName = styled.div({
 const CardButtonsAndQuantidade = styled.div({
     width: '80px',
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    justifyContent: 'center'
 })
 
 const CardButtons = styled.div({
@@ -57,8 +62,53 @@ const CardPrice = styled.div({
     padding: '30px 15px',
     fontSize: '14px',
     fontWeight: 700
-
 })
+
+const CardButtonsAndPrice = styled.div({
+    display: 'flex',
+    
+})
+
+const BtnClosedCardMenu = styled.div({
+    position: 'absolute',
+    right: '-6px',
+    top: '-6px',
+    width: '15px',
+    height: '15px',
+})
+
+const SvgClosedCardMenu = (props: {
+    methodClosed: () => void
+}) => {
+    return(
+        <motion.svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                initial="hidden"
+                animate="visible"
+                onClick={() => props.methodClosed()}
+                style={{backgroundColor: 'black', borderRadius: '100px', margin: '0px 0px 0px 0px', alignSelf: 'center'}}
+            >
+                <motion.line
+                    x1="5"
+                    y1="5"
+                    x2="10"
+                    y2="10"
+                    stroke="#fff"
+                    custom={2}
+                    />
+                <motion.line
+                    x1="5"
+                    y1="10"
+                    x2="10"
+                    y2="5"
+                    stroke="#fff"
+                    custom={2.5}
+                    />
+            </motion.svg>
+    )
+}
 
 const CardMenu = (props: {
     card: ProdutoInterface
@@ -66,91 +116,67 @@ const CardMenu = (props: {
 
     const {produtos, setProdutos} = useContext(ProdutosContext)
 
-
-    const formatPrice = `${Number(props.card.price).toLocaleString('pt-BR', {
-        style: 'currency', 
-        currency: 'BRL'
-    })}`
-
-
-    const findItem = (): ProdutoInterface | undefined => {
-        return produtos.find(produto => produto?.id ===  props?.card?.id)
-    }
-    const soma = () => {
-        const item = findItem()
-       if(!item) {
-            return;
-       }
-        if(!item?.qtd){
-            item.qtd = 1
-            setProdutos([...produtos])
-            return;
-        }
-        item.qtd++
-       setProdutos([...produtos])
-    }
-
-    const subtrai = () => {
-        const item = findItem()
-       if(!item) {
-            return;
-       }
-        if(!item?.qtd){
-            return;
-        }
-        item.qtd--
-       setProdutos([...produtos])
+    const excluirCardMenu = () => {
+        const item = findItem(produtos, props.card.id)
+        resetQtdItem(item)
+        setProdutos([...produtos])
     }
 
     return (
         <CardMenuStyled className="mobile-card">
+            <BtnClosedCardMenu>
+                <SvgClosedCardMenu methodClosed={excluirCardMenu.bind(this)}/>
+            </BtnClosedCardMenu>
             <Image src={props.card.photo} alt={props.card.name} width={80} height={80}/>
-            <CardName>
+            <CardName className="card-titulo">
                 {props.card.name}
             </CardName>
-            <CardButtonsAndQuantidade>
-                <div>
-                    Qtd
+            <CardButtonsAndPrice className="card-buttons-price">
+                <CardButtonsAndQuantidade className="card-buttons-qtd">
+                    <div className="qtd">
+                        Qtd
+                    </div>
+                    <CardButtons>
+                        <CardButton
+                            onClick={()=> somaMaisUm(produtos, props.card.id, setProdutos.bind(this))}
+                            >
+                                +
+                        </CardButton>
+                            <hr/>
+                        <CardButton>
+                                {props.card.qtd}
+                        </CardButton>
+                        <hr />
+                        <CardButton
+                            onClick={()=> subtraiUm(produtos, props.card.id, setProdutos.bind(this))}
+                            >
+                                -
+                        </CardButton>
+                    </CardButtons>
+                </CardButtonsAndQuantidade>
+                <div className="card-price">
+                    <CardPrice className="mobile-hidden">
+                        { formatPrice(props.card.price)}
+                    </CardPrice>
+                    <SpanPrice className="mobile-show">{ formatPrice(props.card.price)}</SpanPrice>
                 </div>
-                <CardButtons>
-                    <CardButton
-                        onClick={()=> soma()}
-                        >
-                            +
-                    </CardButton>
-                        <hr/>
-                    <CardButton>
-                            {props.card.qtd}
-                    </CardButton>
-                    <hr />
-                    <CardButton
-                        onClick={()=> subtrai()}
-                        >
-                            -
-                    </CardButton>
-                </CardButtons>
-            </CardButtonsAndQuantidade>
-            <CardPrice>
-                {formatPrice}
-            </CardPrice>
+            </CardButtonsAndPrice>
         </CardMenuStyled>
     )
 }
 
-const TituloMenuAndClose = (props: {
+const BtnClosedMenu = (props: {
     isOpen: boolean,
     methodIsOpen: Dispatch<SetStateAction<boolean>>
 }) => {
     return(
-        <TituloMenuAndCloseStyle>
-            <TituloMenu>Carrinho<br /> de Compras</TituloMenu>
-            <motion.svg
+        <motion.svg
                 width="40"
                 height="40"
                 viewBox="0 0 60 60"
                 initial="hidden"
                 animate="visible"
-                style={{backgroundColor: 'black', borderRadius: '100px', margin: '0px 20px 0px 0px', alignSelf: 'center'}}
+                style={{backgroundColor: 'black', borderRadius: '100px', margin: '0px 20px 0px 20px', alignSelf: 'center'}}
                 onClick={() => props.methodIsOpen(!props.isOpen)}
             >
                 <motion.line
@@ -170,13 +196,38 @@ const TituloMenuAndClose = (props: {
                     custom={2.5}
                     />
             </motion.svg>
+    )
+}
+
+const TituloMenuAndClose = (props: {
+    isOpen: boolean,
+    methodIsOpen: Dispatch<SetStateAction<boolean>>
+}) => {
+    return(
+        <TituloMenuAndCloseStyle>
+            <TituloMenu>Carrinho<br /> de Compras</TituloMenu>
+            <BtnClosedMenu isOpen={props.isOpen} methodIsOpen={props.methodIsOpen}/>
         </TituloMenuAndCloseStyle>
     )
 }
 
 const variants: Variants = {
     active: { opacity: 1, x: 0, height: '100vh'},
-    defaultConfig: { opacity: 0, x: "100%" ,backgroundColor: '#0F52BA', height: '100vh', position: 'fixed', right: 0, top: 0, width: '486px', boxShadow: '-5px 0px 6px 0px #00000021', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', color: '#fff'}
+    defaultConfig: { 
+        opacity: 0, 
+        x: "100%" ,
+        backgroundColor: '#0F52BA',
+        height: '100vh',
+        position: 'fixed',
+        right: 0,
+        top: 0,
+        width: '486px',
+        boxShadow: '-5px 0px 6px 0px #00000021',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        color: '#fff'
+    }
 }
 
 
@@ -185,7 +236,8 @@ const CardContainerLayout = styled.div({
     maxHeight: '52vh',
     overflowX: 'auto',
     scrollbarWidth: 'thin',
-    transform: 'translateY(180deg)'
+    transform: 'translateY(180deg)',
+    padding: '0px 10px'
 })
 
 const ButtonFinalizarStyled = styled.div({
@@ -210,18 +262,13 @@ const TotalCompraStyled = styled.div({
 const TotalCompra = (props: {
     total: number
 }) => {
-    const formatPrice = `${Number(props.total).toLocaleString('pt-BR', {
-        style: 'currency', 
-        currency: 'BRL'
-    })}`
-
     return (
         <TotalCompraStyled>
             <div >
                 Total:
             </div>
             <div>
-                {formatPrice}
+                {formatPrice(props.total)}
             </div>
         </TotalCompraStyled>
     )
@@ -240,6 +287,10 @@ const ButtonFinalizar = (props: {
     )
 }
 
+const MenuContainerStyled = styled.div({
+    padding: '20px 20px 0px 20px'
+})
+
 const Menu = (props: {
     isOpen: boolean,
     methodIsOpen: Dispatch<SetStateAction<boolean>>
@@ -253,13 +304,14 @@ const Menu = (props: {
             animate={props.isOpen ? 'active' : ''}
             variants={variants}
             initial="defaultConfig"
+            className="menu-mobile"
             >
-                <div style={{padding: '20px 0px 0px 60px'}}>
+                <MenuContainerStyled className="menu-mobile">
                     <TituloMenuAndClose  isOpen={props.isOpen}  methodIsOpen={props.methodIsOpen}/>
                     <CardContainerLayout>
                         {produtosComCompra.map((prd, i) => <CardMenu key={i} card={prd}/>)}
                     </CardContainerLayout>
-                </div>
+                </MenuContainerStyled>
                 <ButtonFinalizar total={totalCompra}/>
         </motion.nav>
     )
